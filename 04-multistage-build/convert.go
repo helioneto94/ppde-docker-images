1e57a659577dff1fc588418757ab930379419cd1
Helio Neto <helioneto94@gmail.com>
2025-04-10 16:37:41 +0000

    mudança para ffmpeg-go

--- 04-multistage-build/convert.go
+++ 04-multistage-build/convert.go
@@ -1,36 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
+	"fmt"
+	"os"
+
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func main() {
-    if len(os.Args) != 3 {
-        fmt.Println("Uso: conversor <arquivo de entrada> <formato de saída>")
-        fmt.Println("Exemplo: conversor video.mp4 avi")
-        os.Exit(1)
-    }
+	if len(os.Args) < 3 {
+		fmt.Println("Uso: go run main.go <arquivo de entrada> <formato de saída>")
+		os.Exit(1)
+	}
 
-    inputFile := os.Args[1]
-    outputFormat := os.Args[2]
-    outputFile := "output." + outputFormat
+	inputFile := os.Args[1]
+	outputFormat := os.Args[2]
+	outputFile := fmt.Sprintf("output.%s", outputFormat)
 
-    if _, err := os.Stat(inputFile); os.IsNotExist(err) {
-        fmt.Printf("Erro: O arquivo '%s' não foi encontrado!\n", inputFile)
-        os.Exit(1)
-    }
+	fmt.Printf("Convertendo %s para %s...\n", inputFile, outputFile)
 
-    fmt.Printf("Iniciando conversão de '%s' para '%s'...\n", inputFile, outputFile)
-    cmd := exec.Command("ffmpeg", "-i", inputFile, "-preset", "fast", "-c:v", "libx264", "-c:a", "aac", outputFile)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	err := ffmpeg.Input(inputFile).
+		Output(outputFile).
+		OverWriteOutput().
+		Run()
 
-    if err := cmd.Run(); err != nil {
-        fmt.Println("Erro na conversão do arquivo.")
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Printf("Erro na conversão: %v\n", err)
+		os.Exit(1)
+	}
 
-    fmt.Printf("Conversão concluída com sucesso! Arquivo gerado: %s\n", outputFile)
+	fmt.Printf("Conversão finalizada! Arquivo gerado: %s\n", outputFile)
 }
